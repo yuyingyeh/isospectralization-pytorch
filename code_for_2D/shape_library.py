50c97e1fca603bd0d15abab35964e742cb200501
yuyingyeh <yuying1423@gmail.com>
2020-02-17 18:15:45 -0800

    modified to PyTorch version

--- code_for_2D/shape_library.py
+++ code_for_2D/shape_library.py
@@ -22,14 +22,14 @@ from scipy.spatial.distance import cdist
 
 def totuple(a):
     return [ tuple(i) for i in a]
-        
+
 def plotly_trisurf(V, TRIV):
     p3.clear()
     p3.plot_trisurf(V[:,0], V[:,1], V[:,2], triangles=TRIV)
     p3.scatter(V[:,0], V[:,1], V[:,2], marker='sphere', color='blue', size=0.33)
     p3.squarelim()
     p3.show()
-    
+
 import time
 
 def TicTocGenerator():
@@ -53,10 +53,12 @@ def toc(tempBool=True):
 def tic():
     # Records a time in TicToc, marks the beginning of a time interval
     toc(False)
-    
+
+def pteval(X):
+
 def tfeval(X):
     gpu_options = tf.GPUOptions(allow_growth = True)
-    config = tf.ConfigProto(device_count={'CPU': 1, 'GPU': 0}, 
+    config = tf.ConfigProto(device_count={'CPU': 1, 'GPU': 0},
     allow_soft_placement = False, gpu_options=gpu_options)
     sess = tf.Session(config=config)
     tf.global_variables_initializer().run(session=sess)
@@ -68,7 +70,7 @@ def tfeval(X):
 
 def tfeig(X):
     gpu_options = tf.GPUOptions(allow_growth = True)
-    config = tf.ConfigProto(device_count={'CPU': 1, 'GPU': 0}, 
+    config = tf.ConfigProto(device_count={'CPU': 1, 'GPU': 0},
     allow_soft_placement = False, gpu_options=gpu_options)
     sess = tf.Session(config=config)
     tf.global_variables_initializer().run(session=sess)
@@ -76,7 +78,7 @@ def tfeig(X):
     #[evals, evecs]  = sess.run( [tfEvals, tfEvecs] );
     LAP = sess.run(tf.identity(X) );
     sess.close();
-    
+
     [evals, evecs] = scipy.linalg.eigh(LAP);
     evals = np.diag(evals)
     return evals, evecs, LAP
@@ -84,33 +86,33 @@ def tfeig(X):
 def load_mesh(path):
     VERT = np.loadtxt(path+'/mesh.vert')
     TRIV = np.loadtxt(path+'/mesh.triv',dtype='int32')-1
-    
+
     return VERT, TRIV
 
 def totuple(a):
     return [ tuple(i) for i in a]
-    
+
 def save_ply(V,T,filename):
     if(V.shape[1]==2):
         Vt = np.zeros((V.shape[0],3))
         Vt[:,0:2] = V
         V = Vt
-        
+
     vertex = np.array(totuple(V),dtype=[('x', 'f4'), ('y', 'f4'),('z', 'f4')])
 #     print([ tuple([i]) for i in T])
     face = np.array([ tuple([i]) for i in T],dtype=[('vertex_indices', 'i4', (3,))])
     el1 = PlyElement.describe(vertex, 'vertex')
     el2 = PlyElement.describe(face, 'face')
     PlyData([el1,el2]).write(filename)
-    #PlyData([el2]).write('some_binary.ply')    
-    
+    #PlyData([el2]).write('some_binary.ply')
+
 def load_ply(fname):
     plydata = PlyData.read(fname)
     VERT = np.asarray([ (v[0],v[1],v[2]) for v in plydata.elements[0].data])
     TRIV = np.asarray([ t[0] for t in plydata.elements[1].data])
     return VERT,TRIV
 
-    
+
 def ismember(T, pts):
     out = np.zeros(np.shape(T)[0])
     for r in range(np.shape(T)[0]):
@@ -131,14 +133,14 @@ def prepare_mesh(VERT,TRIV,dtype='float32'):
 
     def _setedg(i,j,k):
         edges_count[i,j] +=1
-        if edges[i,j,0]==k: 
+        if edges[i,j,0]==k:
             return
-        if edges[i,j,1]==k: 
+        if edges[i,j,1]==k:
             return
-        if edges[i,j,0]==-1: 
+        if edges[i,j,0]==-1:
     #         print(edges[i,j,0])
             edges[i,j,0]=k
-        else:        
+        else:
             edges[i,j,1]=k
 
 
@@ -163,7 +165,7 @@ def prepare_mesh(VERT,TRIV,dtype='float32'):
             map_[j,i]  = idx;
             invmap[idx,:] = [i,j]
             iM[idx,i] = 1;
-            iM[idx,j] = -1;   
+            iM[idx,j] = -1;
             bound_edges[idx,0] = edges_count[i,j]<2
             idx=idx+1;
 #     print(idx)
@@ -177,8 +179,8 @@ def prepare_mesh(VERT,TRIV,dtype='float32'):
     Ik_k  = np.zeros(shape=(m,m,3),dtype=dtype);
     Ih_k  = np.zeros(shape=(m,m,3),dtype=dtype);
     for i in range(n):
-        for j in range(i+1,n):        
-            if(edges[i,j,0]==-1): continue        
+        for j in range(i+1,n):
+            if(edges[i,j,0]==-1): continue
 
             k = edges[i,j,0]
             Ik[map_[i,j],map_[i,j]]=-1;
@@ -189,7 +191,7 @@ def prepare_mesh(VERT,TRIV,dtype='float32'):
             Ik_k[map_[i,j],map_[j,k],1] = 1;
             Ik_k[map_[i,j],map_[k,i],2] = 1;
 
-            if(edges[i,j,1]==-1): continue    
+            if(edges[i,j,1]==-1): continue
 
             k = edges[i,j,1]
             Ih[map_[i,j],map_[i,j]]=-1;
@@ -211,14 +213,14 @@ def prepare_mesh(VERT,TRIV,dtype='float32'):
         Txi[i,TRIV[i,2]] =  1;
 
     Windices = np.zeros(shape=(m,2),dtype=dtype)
-    for i in range(m):    
+    for i in range(m):
         #Windices[i,:] = [invmap[i,0],invmap[i,1]];
         Windices[i,:] = [invmap[i,0]*n+invmap[i,1], i];
 
 
     def calc_adj_matrix(VERT,TRIV):
         n = np.shape(VERT)[0]
-        A = np.zeros((n,n))    
+        A = np.zeros((n,n))
         A[TRIV[:,0],TRIV[:,1]] = 1
         A[TRIV[:,1],TRIV[:,2]] = 1
         A[TRIV[:,2],TRIV[:,0]] = 1
@@ -245,7 +247,7 @@ def prepare_mesh(VERT,TRIV,dtype='float32'):
         if(nx_e[0] != ord_list[-1][1]):
             nx_e = nx_e[[1,0]]
         ord_list.append(nx_e)
-    ord_list=np.asarray(ord_list)  
+    ord_list=np.asarray(ord_list)
 
     return np.asarray(VERT,dtype),TRIV, n, m, Ik, Ih, Ik_k, Ih_k, Tpi, Txi, Tni, iM, Windices, Ael, Bary, bound_edges, ord_list
 
@@ -257,14 +259,14 @@ def fps_euclidean(X,nsamp, seed=1):
     for i in range(np.size(seed),nsamp):
         d = np.min(cdist(X,pts),axis=1)
         index_max = np.argmax(d)
-        pts[i,:] = X[index_max,:]; 
+        pts[i,:] = X[index_max,:];
     return pts
 
 
 def resample(VERT, TRIV, npts=-1):
     if(npts==-1):
         npts=int(np.shape(VERT)[0])
-    
+
     minx = np.min(VERT)
     maxx = np.max(VERT)
 
@@ -275,42 +277,42 @@ def resample(VERT, TRIV, npts=-1):
 
     xv = xv + (np.random.rand(xv.shape[0])-0.5)*0.9*(maxx-minx)/dpts
     yv = yv + (np.random.rand(yv.shape[0])-0.5)*0.9*(maxx-minx)/dpts
-    
+
     mesh = prepare_mesh(VERT,TRIV)
     [VERT, TRIV, n, m, Ik, Ih, Ik_k, Ih_k, Tpi, Txi, Tni, iM, Windices, Ael, Bary, bound_edges, ord_list] = mesh
-    
+
     pts = np.stack((xv,yv),axis=1)
-    
+
     p = path.Path(VERT[ord_list[:,0],:2])  # square with legs length 1 and bottom left corner at the origin
     inside = p.contains_points(pts)
 
     #resample boundary
     ord_list = np.vstack([ord_list, ord_list[:1,:]])
-    
+
     bound_pts = [VERT[ord_list[0,0],:2]]
     for i in range(1,ord_list.shape[0]):
         sp = bound_pts[-1]
         pv = VERT[ord_list[i,0],:2]-sp
         l = np.linalg.norm(pv)
-        toadd = int(l/0.05)        
+        toadd = int(l/0.05)
         for j in range(1,toadd+1):
             pta = sp+pv*j/toadd
             bound_pts.append(pta)
 
-    bound_pts = np.asarray(bound_pts) 
+    bound_pts = np.asarray(bound_pts)
 
     pts = np.concatenate( (bound_pts,pts[inside,:] ), axis=0)
     pts = fps_euclidean(pts, npts, np.asarray(range(bound_pts.shape[0])) )
 
     sg = np.stack( ( np.asarray(range(bound_pts.shape[0]-1)),np.asarray(range(1,bound_pts.shape[0])) ), axis=1)
     sg = np.concatenate( (sg, [ [bound_pts.shape[0]-1, 0]] ) )
-    
+
     dt = vispy.geometry.Triangulation(pts, sg)
     dt.triangulate()
 
     VV = dt.pts
     TT = dt.tris
-    
+
     valid_idx = np.unique(TT)
     vV = VV[valid_idx,:]
     map_v = np.ones( (VV.shape[0]), np.int32)*-1
--- code_for_2D/spectrum_alignment.py
+++ code_for_2D/spectrum_alignment.py
@@ -2,33 +2,41 @@ from scipy import sparse
 #import matplotlib.pyplot as plt
 import os
 import sys
-import tensorflow as tf
+#import tensorflow as tf
+import torch
+from torch.autograd import Variable
+import torch.optim as optim
+import torchvision.utils as vutils
+import torch.nn as nn
+from torch.utils.data import DataLoader
+import torch.nn.functional as F
+
 import numpy as np
 import scipy
 from shape_library import *
 from IPython.display import clear_output
 
 import matplotlib.pyplot as plt
-    
+
 class OptimizationParams:
     def __init__(self, smoothing='displacement'):
-        
+
         self.checkpoint = 100
         self.plot=False
-        
+
         self.evals = [20]
         self.numsteps = 5000
         self.remesh_step = 500
-        
+
         self.decay_target = 0.05
         self.learning_rate = 0.005
         self.min_eval_loss = 0.05
-        
+
         self.flip_penalty_reg = 1e10
         self.inner_reg = 1e0
         self.bound_reg = 2e1
-        
-def tf_calc_lap(mesh,VERT): 
+
+def tf_calc_lap(mesh,VERT):
     [_,TRIV,n, m, Ik, Ih, Ik_k, Ih_k, Tpi, Txi, Tni, iM, Windices, Ael, Bary, bound_edges, ord_list] = mesh
     dtype='float32'
     if(VERT.dtype=='float64'):
@@ -36,85 +44,96 @@ def tf_calc_lap(mesh,VERT):
     if(VERT.dtype=='float16'):
         dtype='float16'
 
-    L2 = tf.expand_dims(tf.reduce_sum(tf.matmul(iM,VERT)**2,axis=1),axis=1)
-    L=tf.sqrt(L2);
+    #L2 = tf.expand_dims(tf.reduce_sum(tf.matmul(iM,VERT)**2,axis=1),axis=1)
+    L2 = torch.unsqueeze(torch.sum(torch.mm(iM,VERT)**2,dim=1),dim=1)
+    #L=tf.sqrt(L2);
+    L=torch.sqrt(L2);
 
     def  fAk(Ik,Ik_k):
         Ikp=np.abs(Ik);
-        Sk = tf.matmul(Ikp,L)/2    
-        SkL = Sk-L;    
-        Ak = Sk*(tf.matmul(Ik_k[:,:,0],Sk)-tf.matmul(Ik_k[:,:,0],L))\
-                       *(tf.matmul(Ik_k[:,:,0],Sk)-tf.matmul(Ik_k[:,:,1],L))\
-                       *(tf.matmul(Ik_k[:,:,0],Sk)-tf.matmul(Ik_k[:,:,2],L))
-        return tf.sqrt(tf.abs(Ak)+1e-20)
+        #Sk = tf.matmul(Ikp,L)/2
+        Sk = torch.mm(Ikp,L)/2
+        SkL = Sk-L;
+        Ak = Sk*(torch.mm(Ik_k[:,:,0],Sk)-torch.mm(Ik_k[:,:,0],L))\
+                       *(torch.mm(Ik_k[:,:,0],Sk)-torch.mm(Ik_k[:,:,1],L))\
+                       *(torch.mm(Ik_k[:,:,0],Sk)-torch.mm(Ik_k[:,:,2],L))
+        return torch.sqrt(torch.abs(Ak)+1e-20)
 
     Ak = fAk(Ik,Ik_k)
     Ah = fAk(Ih,Ih_k)
 
     #sparse representation of the Laplacian matrix
-    W = -tf.matmul(Ik,L2)/(8*Ak)-tf.matmul(Ih,L2)/(8*Ah);
+    W = -torch.mm(Ik,L2)/(8*Ak)-torch.mm(Ih,L2)/(8*Ah);
 
 
     #compute indices to build the dense Laplacian matrix
-    Windtf = tf.SparseTensor(indices=Windices, values=-np.ones((m),dtype), dense_shape=[n*n, m])
-    Wfull  = -tf.reshape(tf.sparse_tensor_dense_matmul(Windtf,W),(n,n))
-    Wfull = (Wfull + tf.transpose(Wfull))
+    #Windtf = tf.SparseTensor(indices=Windices, values=-np.ones((m),dtype), dense_shape=[n*n, m])
+    Windtf = torch.sparse.FloatTensor(torch.LongTensor(Windices).t(), \
+                                      torch.FloatTensor(-np.ones((m),dtype)), \
+                                      torch.Size([n*n, m]))
+    #Wfull  = -tf.reshape(tf.sparse_tensor_dense_matmul(Windtf,W),(n,n))
+    Wfull  = -torch.reshape(torch.mm(Windtf,W),(n,n))
+    #Wfull = (Wfull + tf.transpose(Wfull))
+    Wfull = (Wfull + torch.t(Wfull))
 
     #actual Laplacian
-    Lx = Wfull-tf.diag(tf.reduce_sum(Wfull,axis=1))
-    S = (tf.matmul(Ael,Ak)+tf.matmul(Ael,Ah))/6;
-    
+    #Lx = Wfull-tf.diag(tf.reduce_sum(Wfull,axis=1))
+    Lx = Wfull-torch.diag(torch.sum(Wfull,dim=1))
+    S = (torch.mm(Ael,Ak)+torch.mm(Ael,Ah))/6;
+
     return Lx,S,L,Ak;
- 
-    
+
+
 def calc_evals(VERT,TRIV):
     mesh = prepare_mesh(VERT,TRIV)
     Lx,S,L,Ak = tf_calc_lap(mesh,mesh[0])
-    Si = tf.diag(tf.sqrt(1/S[:,0]))
-    Lap =  tf.matmul(Si,tf.matmul(Lx,Si));
-    [evals,evecs]  = tf.self_adjoint_eig( Lap )
-    return tfeval(evals)
+    Si = torch.diag(torch.sqrt(1/S[:,0]))
+    Lap =  torch.mm(Si,torch.mm(Lx,Si));
+    #[evals,evecs]  = tf.self_adjoint_eig( Lap )
+    evals = torch.symeig( Lap )
+    #return tfeval(evals)
+    return pteval(evals)
 
-    
-def build_graph(mesh, evals, nevals, step=1.0, params=OptimizationParams()): 
+
+def build_graph(mesh, evals, nevals, step=1.0, params=OptimizationParams()):
         """Build the tensorflow graph
-        
+
         Input arguments:
         - mesh: structure representing the triangulated mesh
         - nevals: number of eigenvalues to optimize
         """
         graph = lambda: None
-        
+
         [Xori,TRIV,n, m, Ik, Ih, Ik_k, Ih_k, Tpi, Txi, Tni, iM, Windices, Ael, Bary, bound_edges, ord_list] = mesh
         dtype='float32'
         if(Xori.dtype=='float64'):
             dtype='float64'
         if(Xori.dtype=='float16'):
             dtype='float16'
-            
-            
+
+
         #setup cosine decay
         global_step = tf.Variable(step+1.0, name='global_step',trainable=False)
-        
+
         graph.global_step_val = tf.placeholder(dtype)
         graph.set_global_step = tf.assign(global_step, graph.global_step_val).op
-        
+
         cosine_decay = 0.5 * (1 + tf.cos(3.14 * tf.minimum(np.asarray(params.numsteps/2.0,dtype=dtype),global_step) / (params.numsteps/2.0)))
         graph.decay= (1 - params.decay_target) * cosine_decay + params.decay_target
-               
-        
+
+
         #model the shape deformation as a displacement vector field
         bound_vert = np.zeros((n,1),np.float32)
         bound_vert[ord_list] = 1
-        
+
         dXb = tf.Variable((0*Xori).astype(dtype));
         dXi = tf.Variable((0*Xori).astype(dtype));
         scaleX = tf.Variable(1,dtype=dtype); #not used in shape alignment
-       
-    
-        graph.input_X = tf.placeholder(shape=dXb.shape,dtype=dtype);        
+
+
+        graph.input_X = tf.placeholder(shape=dXb.shape,dtype=dtype);
         graph.assign_X = tf.assign(dXb, graph.input_X-Xori*scaleX).op;
-                
+
         graph.X=(Xori + dXb*bound_vert + dXi*(1-bound_vert))*scaleX;
 
         Lx,S,L,Ak = tf_calc_lap(mesh,graph.X)
@@ -122,11 +141,11 @@ def build_graph(mesh, evals, nevals, step=1.0, params=OptimizationParams()):
         #Normalized Laplacian
         Si = tf.diag(tf.sqrt(1/S[:,0]))
         Lap =  tf.matmul(Si,tf.matmul(Lx,Si));
-        
+
         #Spectral decomposition
         [graph.evals,v]  = tf.self_adjoint_eig( Lap )
         graph.cost_evals = 1e1*tf.nn.l2_loss( (graph.evals[0:nevals]-evals[0:nevals]) * (1/np.asarray(range(1,nevals+1),'float32'))) # \
-#       
+#
         # triangle flip penalty
         tp = tf.matmul(Tpi[:,:],graph.X)
         tx = tf.matmul(Txi[:,:],graph.X)
@@ -135,7 +154,7 @@ def build_graph(mesh, evals, nevals, step=1.0, params=OptimizationParams()):
         cp = tf.reduce_sum(tf.matmul(tn,Rot)*(tx-tp),axis=1)
         graph.cp = cp-1e-4
         graph.flip_cost =  params.flip_penalty_reg*tf.nn.l2_loss(graph.cp-tf.abs(graph.cp))
-        
+
         #inner points regularizer
         meanA, varA = tf.nn.moments(Ak, axes=[0])
         graph.inner_reg_cost = params.inner_reg*(tf.nn.l2_loss(L) + tf.nn.l2_loss(varA));
@@ -145,26 +164,29 @@ def build_graph(mesh, evals, nevals, step=1.0, params=OptimizationParams()):
         #inner and outer points cost functions
         graph.cost_bound = graph.cost_evals + graph.flip_cost + graph.bound_reg_cost
         graph.cost_inner = graph.inner_reg_cost + graph.flip_cost
-        
+
         optimizer = tf.train.AdamOptimizer(params.learning_rate)
-        
+
         def clipped_grad_minimize(cost, variables):
             gvs = optimizer.compute_gradients(cost, var_list=variables)
             clipped_gvs = [(tf.clip_by_value(grad, -0.0001, 0.0001), var) for grad, var in gvs if grad!=None]
             return optimizer.apply_gradients(clipped_gvs,global_step=global_step)
-        
+
         graph.train_op_bound = clipped_grad_minimize(graph.cost_bound, [dXb])
         graph.train_op_inner = clipped_grad_minimize(graph.cost_inner, [dXi])
-                
+
         return graph
-        
-    
-    
+
+
+
 def run_optimization(mesh, target_evals, out_path, params = OptimizationParams() ):
-    
+
+    # GPU options
+    '''
     gpu_options = tf.GPUOptions(allow_growth = True)
     config = tf.ConfigProto(device_count={'CPU': 1, 'GPU': 1}, allow_soft_placement = False, gpu_options=gpu_options)
     config.gpu_options.allow_growth=True
+    '''
 
     try:
         os.makedirs('%s/' % (out_path))
@@ -172,27 +194,29 @@ def run_optimization(mesh, target_evals, out_path, params = OptimizationParams()
         pass
 
     [Xopt,TRIV,n, m, Ik, Ih, Ik_k, Ih_k, Tpi, Txi, Tni, iM, Windices, Ael, Bary, bound_edges, ord_list] = mesh
-    
+
     iterations = [];
     for nevals in params.evals:
       tf.reset_default_graph()
-      with tf.Session(config=config) as session:   
-        
-        step=0    
+      with tf.Session(config=config) as session:
+
+        step=0
         while(step<params.numsteps-1):
           mesh = prepare_mesh(Xopt,TRIV)
-         
+
           [Xori,TRIV,n, m, Ik, Ih, Ik_k, Ih_k, Tpi, Txi, Tni, iM, Windices, Ael, Bary, bound_edges, ord_list] = mesh
           edg_v = np.zeros((n,1),np.float32)
           edg_v[ord_list] = 1
 
           #Buil tensorflow graph
-          graph = build_graph(mesh,target_evals, nevals,step)#,smoothing,numsteps,edg_v)
-          tf.global_variables_initializer().run()
+          #graph = build_graph(mesh,target_evals, nevals,step)#,smoothing,numsteps,edg_v)
+          #tf.global_variables_initializer().run()
+
+          #Initialize Model
 
           tic()
           for step in range(step+1,params.numsteps):
-            
+
             if((step)%params.remesh_step==0):
                 print("RECOMPUTING TRIANGULATION at step %d" % step)
                 break;
@@ -205,18 +229,18 @@ def run_optimization(mesh, target_evals, out_path, params = OptimizationParams()
                     _, er, ee, Xopt_t = session.run([graph.train_op_inner,graph.cost_inner,graph.cost_evals,graph.X], feed_dict=feed_dict)
                 else:
                     _, er, ee, Xopt_t = session.run([graph.train_op_bound,graph.cost_bound,graph.cost_evals,graph.X], feed_dict=feed_dict)
-                
+
                 iterations.append((step, nevals, er, ee,int(step/10)%2))
-                
-                
-                if ( (step) % params.checkpoint == 0 or step==(params.numsteps-1) or step==1): 
+
+
+                if ( (step) % params.checkpoint == 0 or step==(params.numsteps-1) or step==1):
                     toc()
                     tic()
 
                     cost, cost_evals, cost_vcL, cost_vcW, decay, flip, evout = session.run([graph.cost_bound, graph.cost_evals, graph.inner_reg_cost,graph.bound_reg_cost, graph.decay, graph.cp,graph.evals], feed_dict=feed_dict)
 
                     print('Iter %f, cost: %f(evals cost: %f (%f) (%f), smoothness weight: %f). Flip: %d' %
-                          (step, cost, cost_evals, cost_vcL, cost_vcW, decay, np.sum(flip<0)))  
+                          (step, cost, cost_evals, cost_vcL, cost_vcW, decay, np.sum(flip<0)))
 
                     if params.plot:
 #                         fig=plt.figure(figsize=(9, 4), dpi= 80, facecolor='w', edgecolor='k')
@@ -224,10 +248,10 @@ def run_optimization(mesh, target_evals, out_path, params = OptimizationParams()
                         plt.plot(evout[0:nevals],'-b')
                         plt.show()
 
-                        plt.triplot(Xopt[:,0],Xopt[:,1],TRIV)   
+                        plt.triplot(Xopt[:,0],Xopt[:,1],TRIV)
                         plt.axis('equal')
                         plt.show()
-                        
+
                     save_ply(Xopt,TRIV,'%s/evals_%d_iter%d.ply' % (out_path,nevals,step))
                     np.savetxt('%s/evals_%d_iter%d.txt' % (out_path,nevals,step),evout)
 
@@ -237,7 +261,7 @@ def run_optimization(mesh, target_evals, out_path, params = OptimizationParams()
                         step=params.numsteps
                         print('Minimum eighenvalues loss reached')
                         break
-                    
+
             except KeyboardInterrupt:
                 step = params.numsteps
                 break;
@@ -249,11 +273,11 @@ def run_optimization(mesh, target_evals, out_path, params = OptimizationParams()
             if(ee!=ee):
               print('iter %d. Perturbating initial condition' % step)
               tf.global_variables_initializer().run()
-              Xopt=Xopt+(np.random.rand(np.shape(Xopt)[0],np.shape(Xopt)[1])-0.5)*1e-3                  
-              _ = session.run(graph.set_global_step, feed_dict = {graph.global_step_val: step})  
+              Xopt=Xopt+(np.random.rand(np.shape(Xopt)[0],np.shape(Xopt)[1])-0.5)*1e-3
+              _ = session.run(graph.set_global_step, feed_dict = {graph.global_step_val: step})
             else:
-              Xopt=Xopt_t     
+              Xopt=Xopt_t
           if(step<params.numsteps-1):
               [Xopt,TRIV] = resample(Xopt, TRIV)
-                
-  
+
+
